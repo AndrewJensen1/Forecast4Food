a4f9362a37f4061fde94db7c9c6c711cd071b888
unknown <josephleeper@gmail.com>
2019-09-09 16:46:30 -0400

    location model, methods

--- Finalproject/Controllers/FinalProjectDbController.cs
+++ Finalproject/Controllers/FinalProjectDbController.cs
@@ -1,16 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Finalproject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Finalproject.Controllers
 {
     public class FinalProjectDbController : Controller
     {
+        private readonly FinalProjectDbContext _context;
+        private readonly IConfiguration _configuration;
+        private readonly string _googleApiKey;
         public IActionResult Index()
         {
             return View();
+        }
+        public static HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://maps.google.com");
+            return client;
+        }
+
+        public async Task<IActionResult> Map(string location)
+        {
+            location.Replace(" ", "+");
+            var client = GetHttpClient();
+            var response = await client.GetAsync($"maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key={_googleApiKey}");
+            var name = await response.Content.ReadAsAsync<Location>();
+            return View(name);
         }
     }
 }
