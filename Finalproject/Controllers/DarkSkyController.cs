356619b507d842d49886ed8a4d287ce38e281b53
unknown <josephleeper@gmail.com>
2019-09-14 11:55:35 -0400

    added one and five year weather

--- Finalproject/Controllers/DarkSkyController.cs
+++ Finalproject/Controllers/DarkSkyController.cs
@@ -37,8 +37,31 @@ namespace Finalproject.Controllers
             var response = await client.GetAsync($"{_darkSkyKey}/{TempData["lat"]},{TempData["lng"]},{TempData["day"]}");
             var result = await response.Content.ReadAsAsync<DarkSky>();
 
+            var responseOneYear = await client.GetAsync($"{_darkSkyKey}/{TempData["lat"]},{TempData["lng"]},{ TempData["yearPast"]}");
+            var resultOneYear = await responseOneYear.Content.ReadAsAsync<DarkSky>();
+
+            var responseFiveYears = await client.GetAsync($"{_darkSkyKey}/{TempData["lat"]},{TempData["lng"]},{TempData["yearsPast"]}");
+            var resultFiveYears = await responseFiveYears.Content.ReadAsAsync<DarkSky>();
+            TempData.Keep("day");
+            TempData.Keep("yearPast");
+            TempData.Keep("yearsPast");
             TempData.Keep("lat");
             TempData.Keep("lng");
+
+            ViewBag.oneYearTemp = resultOneYear.currently.temperature;
+            ViewBag.oneYearHumid = resultOneYear.currently.humidity;
+            ViewBag.oneYearWind = resultOneYear.currently.windSpeed;
+            ViewBag.oneYearPrecip = resultOneYear.currently.precipProbability;
+            ViewBag.oneYearSumm = resultOneYear.currently.summary;
+
+            ViewBag.fiveYearTemp = resultFiveYears.currently.temperature;
+            ViewBag.fiveYearHumid = resultFiveYears.currently.humidity;
+            ViewBag.fiveYearWind = resultFiveYears.currently.windSpeed;
+            ViewBag.fiveYearPrecip = resultFiveYears.currently.precipProbability;
+            ViewBag.fiveYearSumm = resultFiveYears.currently.summary;
+
+
+
 
             return View(result);
         }
